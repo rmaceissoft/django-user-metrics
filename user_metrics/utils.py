d9d0e4ad1f1ee3b151ab57d65f0e8d3111848e1f
Reiner Marquez <rmaceissoft@gmail.com>
2012-01-03 11:30:55 -0500

    added a method to calculate the total of aggregated weeks on MetricWeek model

--- user_metrics/utils.py
+++ user_metrics/utils.py
@@ -1,4 +1,18 @@
 import datetime
 
+from user_metrics.models import MetricWeek
+
 def week_for_date(date):
     return date - datetime.timedelta(days=date.weekday())
+
+
+def total_weeks_aggregated():
+    """
+    return total of weeks aggregates to MetricWeek model
+    """
+    try:
+        first_week = MetricWeek.objects.all().order_by('-date_up')[0].date_up
+    except IndexError:
+        first_week = week_for_date(datetime.date.today())
+    return (datetime.date.today() - first_week).days / 7
+
