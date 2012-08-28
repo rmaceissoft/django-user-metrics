0c1fdf464afeaee4875aef4de461da2ee9dc722d
Reiner Marquez <rmaceissoft@gmail.com>
2012-08-28 18:03:31 -0500

    added mixpanel backend

---
+++ user_metrics/backends/mixpanel.py
@@ -0,0 +1,23 @@
+import base64
+import urllib
+import urllib2
+import json
+
+from user_metrics import settings
+
+
+def put_metric(slug, user, count=1, **kwargs):
+    """ Increment a metric by a given user """
+    if "token" not in kwargs:
+        kwargs["token"] = settings.USER_METRICS_MIXPANEL_TOKEN
+    kwargs['$bucket'] = user.id
+
+    params = {"event": slug, "properties": kwargs}
+    b64_data = base64.b64encode(json.dumps(params))
+
+    data = urllib.urlencode({"data": b64_data})
+    req = urllib2.Request(settings.USER_METRICS_MIXPANEL_API_URLurl, data)
+    for i in xrange(count):
+        response = urllib2.urlopen(req)
+        if response.read() == '0':
+            raise Exception(u'MixPanel returned 0')
--- user_metrics/settings.py
+++ user_metrics/settings.py
@@ -2,3 +2,9 @@ from django.conf import settings
 
 
 USER_METRICS_BACKEND = getattr(settings, 'USER_METRICS_BACKEND', 'user_metrics.backends.db')
+
+# mixpanel backend
+USER_METRICS_MIXPANEL_API_URL = getattr(settings, 'USER_METRICS_MIXPANEL_API_URL', 'http://api.mixpanel.com/track/')
+USER_METRICS_MIXPANEL_TOKEN = getattr(settings, 'USER_METRICS_MIXPANEL_TOKEN', None)
+USER_METRICS_MIXPANEL_API_KEY = getattr(settings, 'USER_METRICS_MIXPANEL_API_KEY', None)
+USER_METRICS_MIXPANEL_API_SECRET = getattr(settings, 'USER_METRICS_MIXPANEL_API_SECRET', None)
