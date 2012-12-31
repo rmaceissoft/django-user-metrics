57db623636a126c515723b43e3a912fa1c2d060d
Reiner Marquez <rmaceissoft@gmail.com>
2012-12-31 16:48:11 -0500

    adding example project

---
+++ example_project/example_project/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'example_project.views.home', name='home'),
+    # url(r'^example_project/', include('example_project.foo.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
